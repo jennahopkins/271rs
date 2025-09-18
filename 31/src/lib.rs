082e548745eed85a3640a7a688dd52ecf9f801ec
laptop <jennahopkins@gmail.com>
2025-09-18 09:32:11 -0700

    Finished lab 31

--- 31/src/lib.rs
+++ 31/src/lib.rs
@@ -1,26 +1,28 @@
+// can i make them mutable?
 pub fn weight_u8(byte: u8) -> u64 {
+    /* Finds the hamming weight of a u8 */
+    let mut byte: u8 = byte.clone();
     let mut count: u64 = 0;
-    let bin_str = format!("{:08b}", byte);
-    for ch in bin_str.chars() {
-        if ch == '1' {
-            count += 1;
-        }
+    while byte != 0 {
+        count += (byte & 1) as u64;
+        byte >>= 1;
     }
     return count;
 }
 
 pub fn weight_u64(word: u64) -> u64 {
+    /* Finds the hamming weight of a u64 */
+    let mut word: u64 = word.clone();
     let mut count: u64 = 0;
-    let bin_str = format!("{:64b}", word);
-    for ch in bin_str.chars() {
-        if ch == '1' {
-            count += 1;
-        }
+    while word != 0 {
+        count += (word & 1) as u64;
+        word >>= 1;
     }
     return count;
 }
 
 pub fn weight_bytes(bytes: Vec<u8>) -> u64 {
+    /* Finds the hamming weight of a vector of u8s */
     let mut count: u64 = 0;
     for byte in bytes {
         count += weight_u8(byte);
@@ -29,9 +31,43 @@ pub fn weight_bytes(bytes: Vec<u8>) -> u64 {
 }
 
 pub fn weight_words(words: Vec<u64>) -> u64 {
+    /* Finds the hamming weight of a vector of u64s */
     let mut count: u64 = 0;
     for word in words {
         count += weight_u64(word);
     }
     return count;
 } 
+
+pub fn distance_u8(a: u8, b: u8) -> u64 {
+    /* Finds the hamming distance between two u8s */
+    let mut count: u64 = 0;
+    count += weight_u8(a ^ b);
+    return count;
+}
+
+pub fn distance_u64(c: u64, d: u64) -> u64 {
+    /* Finds the hamming distance between two u64s */
+    let mut count: u64 = 0;
+    count += weight_u64(c ^ d);
+    return count;
+}
+
+pub fn distance_bytes(a_vec: Vec<u8>, b_vec: Vec<u8>) -> u64 {
+    /* Finds the hamming distance between two vectors of u8s */
+    let mut count: u64 = 0;
+    for i in 0..a_vec.len() {
+        count += distance_u8(a_vec[i], b_vec[i]);
+    }
+    return count;
+}
+
+pub fn distance_words(c_vec: Vec<u64>, d_vec: Vec<u64>) -> u64 {
+    /* Finds the hamming distance between two vectors of u64s */
+    let mut count: u64 = 0;
+    for i in 0..c_vec.len() {
+        count += distance_u64(c_vec[i], d_vec[i]);
+    }
+    return count;
+}
+
--- 31/src/main.rs
+++ 31/src/main.rs
@@ -1,9 +1,15 @@
 fn main() {
+    /* Testing statements for hamming functions in lib.rs */
     dbg!(hamming::weight_u8(0x33_u8)); // should be 4
     dbg!(hamming::weight_u8(255)); // should be 8
     dbg!(hamming::weight_u64(0x1A2B3C4D5E6F7081)); // should be 31
     let bytes: Vec<u8> = vec![72, 101, 108, 108, 111];
-    dbg!(hamming::weight_bytes(bytes)); // should be 20
+    dbg!(hamming::weight_bytes(bytes.clone())); // should be 20
     let words: Vec<u64> = vec![42, 123456789, 999999999999, 18446744073709551615];
-    dbg!(hamming::weight_words(words)); // should be 107
+    dbg!(hamming::weight_words(words.clone())); // should be 107
+    dbg!(hamming::distance_u8(0x33, 255)); // should be 4
+    dbg!(hamming::distance_u64(0x1A2B3C4D5E6F7081, 18446744073709551615)); // should be 33
+    dbg!(hamming::distance_bytes(bytes.clone(), bytes)); // should be 0
+    let words2: Vec<u64> = vec![41, 123456789, 999999999999, 18446744073709551615];
+    dbg!(hamming::distance_words(words, words2)); // should be 2
 }
