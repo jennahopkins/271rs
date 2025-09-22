e65fd68ff7d9f5101f741819e088caed91204132
laptop <jennahopkins@gmail.com>
2025-09-22 10:11:20 -0700

    added rotate functions, need to make compatible with different sizes, get rid of as u64

--- 32/src/lib.rs
+++ 32/src/lib.rs
@@ -1,7 +1,8 @@
+// do i need to specify types? just for what works in assignment?
 #[macro_export]
 macro_rules! choice {
     ($x: expr, $y: expr, $z: expr) => {
-        if $x {$y} else {$z}
+        if $x == 1 {$y} else {$z}
     };
 }
 
@@ -14,9 +15,16 @@ macro_rules! median {
     };
 }
 
-/* #[macro_export]
+#[macro_export]
+macro_rules! rotright {
+    ($x: expr, $y: expr) => {
+        {(($x as u64) >> ($y % size_of_val(&$x))) | (($x as u64) << (size_of_val(&$x) - ($y % size_of_val(&$x))))}
+    };
+} 
+
+#[macro_export]
 macro_rules! rotleft {
     ($x: expr, $y: expr) => {
-    
+        {(($x as u64) << ($y % 64)) | (($x as u64) >> (64 - ($y % 64)))}
     };
-} */
+}
--- 32/src/main.rs
+++ 32/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    /* Various Variables*/
+    let a : [u64; 4] = [0x1111111111110000, 0x1111000011001100, 0x1100110010101010, 0x0123456789ABCDEF];
+
+    println!("CHOICE(\n{:016X},\n{:016X},\n{:016X}) = \n--------\n{:016X}\n\n", 
+             a[0], a[1], a[2], macros::choice!(a[0], a[1], a[2]));
+    println!("MEDIAN(\n{:016X},\n{:016X},\n{:016X}) = \n--------\n{:016X}\n\n", 
+             a[0], a[1], a[2], macros::median!(a[0], a[1], a[2]));
+    println!("*Rotates use a decimal shift value, but print in hexadecimal:\n");
+    println!("ROTATE RIGHT(\n{:016X}, 04) = \n--------\n{:016X}\n\n", a[3],   macros::rotright!(a[3], 4));
+    println!("ROTATE RIGHT(\n{:016X}, 08) = \n--------\n{:016X}\n\n", a[3],   macros::rotright!(a[3], 8));
+    println!("ROTATE RIGHT(\n{:016X}, 12) = \n--------\n{:016X}\n\n", a[3],   macros::rotright!(a[3], 12));
+    println!("ROTATE RIGHT(\n{:016X}, 02) = \n--------\n{:016X}\n\n", 0x1000, macros::rotright!(0x1000, 2));
+    println!("ROTATE RIGHT(\n{:016X}, 30) = \n--------\n{:016X}\n\n", 0x1000, macros::rotright!(0x1000, 30));
+
+    println!("ROTATE LEFT(\n{:016X}, 04) = \n--------\n{:016X}\n\n", a[3],   macros::rotleft!(a[3], 4));
+    println!("ROTATE LEFT(\n{:016X}, 08) = \n--------\n{:016X}\n\n", a[3],   macros::rotleft!(a[3], 8));
+    println!("ROTATE LEFT(\n{:016X}, 12) = \n--------\n{:016X}\n\n", a[3],   macros::rotleft!(a[3], 12));
+    println!("ROTATE LEFT(\n{:016X}, 02) = \n--------\n{:016X}\n\n", 0x1000, macros::rotleft!(0x1000, 2));
+    println!("ROTATE LEFT(\n{:016X}, 30) = \n--------\n{:016X}\n\n", 0x1000, macros::rotleft!(0x1000, 30));
 }
