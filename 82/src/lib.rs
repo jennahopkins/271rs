19659649bd78ff0dfd2fdab0f7a8d84b31e7dab7
laptop <jennahopkins@gmail.com>
2025-10-29 17:47:08 -0700

    finished ed25519

--- 82/src/lib.rs
+++ 82/src/lib.rs
@@ -8,7 +8,6 @@ use sha2::{Digest, Sha512};
 // H(m: bytes) -> bytes
 fn h(m: &[u8]) -> Vec<u8> {
     return Sha512::digest(m).to_vec();
-    //return Sha512::new().chain_update(m).finalize().to_vec();
 }
 
 // bit(h: bytes, i: int) -> int
@@ -50,13 +49,16 @@ pub fn xrecover(y: &BigInt, q: &BigInt, d: &BigInt, i_const: &BigInt) -> BigInt 
 // --- Core Functions (Require Constants) ---
 
 fn edwards(p: &Vec<BigInt>, q_val: &Vec<BigInt>, q: &BigInt, d: &BigInt) -> Vec<BigInt> {
-    let x1 = &p[0];
-    let y1 = &p[1];
-    let x2 = &q_val[0];
-    let y2 = &q_val[1];
-    let x3 = (x1.clone() * y2.clone() + x2.clone() * y1.clone()) * inv(&(BigInt::one() + d * x1.clone() * x2.clone() * y1.clone() * y2.clone()), q);
-    let y3 = (y1.clone() * y2.clone() + x1.clone() * x2.clone()) * inv(&(BigInt::one() - d * x1.clone() * x2.clone() * y1.clone() * y2.clone()), q);
-    return [x3 % q, y3 % q].to_vec();
+    let (x1, y1) = (&p[0], &p[1]);
+    let (x2, y2) = (&q_val[0], &q_val[1]);
+
+    let denom_x = (BigInt::one() + d * x1 * x2 * y1 * y2).rem_euclid(q);
+    let denom_y = (BigInt::one() - d * x1 * x2 * y1 * y2).rem_euclid(q);
+
+    let x3 = ((x1 * y2 + x2 * y1) * inv(&denom_x, q)).rem_euclid(q);
+    let y3 = ((y1 * y2 + x1 * x2) * inv(&denom_y, q)).rem_euclid(q);
+
+    return vec![x3, y3];
 }
 
 fn scalarmult(p: &Vec<BigInt>, e: &BigInt, q: &BigInt, d: &BigInt) -> Vec<BigInt> {
@@ -107,13 +109,13 @@ fn encodepoint(p: &Vec<BigInt>, b: usize) -> Vec<u8> {
 }
 
 pub fn publickey(sk: &[u8], b: usize, q: &BigInt, d: &BigInt, b_point: &Vec<BigInt>) -> Vec<u8> {
-    let h = h(sk);  // h is bytes
-    let a = BigInt::from(1) << (b - 2);
-    let mut sum = BigInt::zero();
-    for i in 3..(b - 2) {
-        sum += (BigInt::from(1) << i) * BigInt::from(bit(&h, i));
-        //sum += BigInt::from(2_i32.pow(i as u32)) * BigInt::from(bit(&h, i));
-    }
+    let h = h(sk); // h is bytes
+    let mut a_bytes = h[..32].to_vec();
+    a_bytes[0] &= 248;
+    a_bytes[31] &= 63;
+    a_bytes[31] |= 64;
+
+    let a = decodeint(&a_bytes, 256);
     let big_a = scalarmult(b_point, &a, q, d);
     return encodepoint(&big_a, b);
 }
@@ -131,10 +133,10 @@ pub fn signature(m: &[u8], sk: &[u8], pk: &[u8], b: usize, q: &BigInt, l: &BigIn
     let h = h(sk);  // h is bytes
     let mut a = BigInt::zero();
     for i in 3..(b - 2) {
-        a += BigInt::from(2_i32.pow(i as u32)) * BigInt::from(bit(&h, i));
+        a += (BigInt::from(2).pow(i as u32)) * BigInt::from(bit(&h, i));
     }
-    a += BigInt::from(2_i32.pow((b - 2) as u32));
-    let mut ex1 = h[b / 2..b].to_vec();
+    a += BigInt::from(2).pow((b - 2) as u32);
+    let mut ex1 = h[h.len() / 2..].to_vec();
     ex1.extend(m);
     let r = hint(&ex1, b);
     let big_r = scalarmult(b_point, &r, q, d);
@@ -162,12 +164,12 @@ fn decodeint(s: &[u8], b: usize) -> BigInt {
     return sum;
 }
 
-fn decodepoint(s: &[u8], b: usize, q: &BigInt, d: &BigInt) -> Vec<BigInt> {
+fn decodepoint(s: &[u8], b: usize, q: &BigInt, d: &BigInt, i_const: &BigInt) -> Vec<BigInt> {
     let mut y = BigInt::zero();
     for i in 0..(b - 1) {
         y += (BigInt::from(1) << i) * BigInt::from(bit(s, i));
     }
-    let mut x = xrecover(&y, q, d, &BigInt::one());
+    let mut x = xrecover(&y, q, d, &i_const);
     if (x.clone() & BigInt::one()) != bit(s, b - 1).into() {
         x = q - x;
     }
@@ -179,19 +181,22 @@ fn decodepoint(s: &[u8], b: usize, q: &BigInt, d: &BigInt) -> Vec<BigInt> {
 }
 
 pub fn checkvalid(s: &[u8], m: &[u8], pk: &[u8], b: usize, q: &BigInt, d: &BigInt, i_const: &BigInt, b_point: &Vec<BigInt>) -> bool {
-    if s.len() != b / 4 {
+    if s.len() != 64 {
         println!("signature length is wrong");
+        return false;
     }
-    if pk.len() != b / 8 {
+    if pk.len() != 32 {
         println!("public-key length is wrong");
+        return false;
     }
 
-    let big_r = decodepoint(&s[0..b / 8], b, q, d);
-    let big_a = decodepoint(&pk, b, q, d);
-    let big_s = decodeint(&s[b / 8..b / 4], b);
+    let big_r = decodepoint(&s[0..32], b, q, d, i_const);
+    let big_a = decodepoint(pk, b, q, d, i_const);
+    let big_s = decodeint(&s[32..], b);
+
     let mut ex = encodepoint(&big_r, b);
     ex.extend(pk);
     ex.extend(m);
     let h = hint(&ex, b);
-    return scalarmult(b_point, &big_s, q, d) == edwards(&big_r, &scalarmult(&big_a, &h, q, d), q, d);
+    scalarmult(b_point, &big_s, q, d) == edwards(&big_r, &scalarmult(&big_a, &h, q, d), q, d)
 }
